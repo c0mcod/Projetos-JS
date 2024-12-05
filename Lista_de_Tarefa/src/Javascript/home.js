2747c2cc9e0e5506bd7b17031fa8fe7bb529c39d
comcod <netosilvaneto6@gmail.com>
2024-12-05 16:24:45 -0300

    Função de alteração de nota

--- Lista_de_Tarefa/src/Javascript/home.js
+++ Lista_de_Tarefa/src/Javascript/home.js
@@ -3,12 +3,14 @@ const modal = document.querySelector('.modal-task')
 const campoNome = document.querySelector('.nameTask');
 const campoDescricao = document.querySelector('.descripTask');
 const containerTask = document.querySelector('.list-tasks');
-const closeModal = document.querySelector('.close');
+const closeModal = document.querySelector('.close-btn');
 const notFoundTask = document.querySelector('.notFound');
+const darkMode = document.querySelector('.dark-btn');
+const findTask = document.querySelector('.findTask');
+const editModal = document.querySelector('.modal-task-edit');
+const submitEdit = document.querySelector('.submit');
 
 function atualizarMensagem() {
-    // botei esse monte de console log pq por algum motivo do demonio não tava sumindo o aviso de "não há tarefas!"
-    // descobri que era pq eu não tinha chamado a função depois de adicionar no evento '-'
     console.log("Número de tarefas no container:", containerTask.children.length);
     if (containerTask.children.length === 0) {
         notFoundTask.classList.remove('hidden');
@@ -19,29 +21,21 @@ function atualizarMensagem() {
     }
 }
 
-
-// Ao clicar no botão de "adicionar tarefa", uma div estilizada apareça e a mensagem de "não há tarefas adicionadas!" suma
 addTarefa.addEventListener('click', () => {
-    // primeira coisa que tem que acontecer para o usuario, é aparecer o modal com as informações que a gente vai usar
     modal.style.display = 'flex';
 })
 
 const submitTask = document.querySelector('.submit')
 submitTask.addEventListener('click', () => {
 
-    // Agora precisamos pegar os VALORES inseridos nos inputs do modal pra trabalhar nas divs que irão ser inseridas com 
-    // esses dados
-    // VALORES guardados nas variaveis nomeInput e descrInput
-    const nomeInput = campoNome.value.trim();
-    const descrInput = campoDescricao.value.trim();
+    var nomeInput = campoNome.value.trim();
+    var descrInput = campoDescricao.value.trim();
 
-    // Agora que temos os valores, precisamos verificar se eles não estão vazios
     if (nomeInput === '' || descrInput === '') {
         alert('Preencha todos os campos!');
         return;
     };
 
-    // Agora que temos os valores e eles não estão vazios, podemos criar a div
     const newTask = document.createElement('div');
     newTask.classList.add('task');
 
@@ -52,6 +46,7 @@ submitTask.addEventListener('click', () => {
             <p>${descrInput}</p>
         <div>
             <button class="delete-task">Excluir</button>
+            <button class="edit-task">Editar</button>
     </div>
         `
 
@@ -67,12 +62,44 @@ submitTask.addEventListener('click', () => {
         newTask.remove();
         atualizarMensagem();
     });
+
+    const editTaskbtn = newTask.querySelector('.edit-task');
+    editTaskbtn.addEventListener('click', () => {
+        editModal.style.display = 'flex';
+
+        const currentName = newTask.querySelector('h3').textContent;
+        const currentDescription = newTask.querySelector('p').textContent;
+
+        document.querySelector('.nameTaskEdit').value = currentName;
+        document.querySelector('.descripTaskEdit').value = currentDescription;
+
+        currentEditingTask = newTask;
+
+        const saveEditBtn = document.querySelector('.submit-edit');
+
+        saveEditBtn.addEventListener("click", () => {
+            const updateName = document.querySelector('.nameTaskEdit').value.trim();
+            const updateDescription = document.querySelector('.descripTaskEdit').value.trim();
+
+            if (updateName === '' || updateDescription === '') {
+                alert('Preencha todos os campos!');
+                return;
+            };
+        
+            currentEditingTask.querySelector('h3').textContent = updateName
+            currentEditingTask.querySelector('p').textContent = updateDescription
+
+            editModal.style.display = 'none';
+        })
+    })
 });
-// função pra fechar o modal 
+
+darkMode.addEventListener("click", () => {
+    const body = document.querySelector('.body');
+    body.style.backgroundColor = '#222831';
+})
+
 closeModal.addEventListener("click", () => {
     modal.style.display = 'none';
 });
-
 atualizarMensagem();
-
-
